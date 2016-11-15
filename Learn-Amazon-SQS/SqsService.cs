f25982d3fcbdcc7eb9d0bbd9d6a2fe3153757e20
Larry Eisenstein <larryeisenstein@gmail.com>
2016-11-14 22:48:14 -0500

    Service added

--- Learn-Amazon-SQS/SqsService.cs
+++ Learn-Amazon-SQS/SqsService.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Amazon.SQS;
+using System.Configuration;
+using Amazon;
+
 
 namespace Learn_Amazon_SQS
 {
     public class SqsService
     {
+        public string QUEUE { get; set; }
+        public RegionEndpoint REGION { get; set; }
+
+        public SqsService()
+        {
+            QUEUE = "";
+            REGION = RegionEndpoint.USEast1;
+        }
     }
 }
