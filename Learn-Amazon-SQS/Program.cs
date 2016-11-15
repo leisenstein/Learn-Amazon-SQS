20e4a33b61bdd0c63cf61338eb57113d56bbf612
Larry Eisenstein <leisenstein@coalitionamerica.com>
2016-11-15 07:41:20 -0500

    set up service

--- Learn-Amazon-SQS/Program.cs
+++ Learn-Amazon-SQS/Program.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Amazon;
+using System.Configuration;
 
 namespace Learn_Amazon_SQS
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        public static string QUEUE { get; set; }
+        public static string QUEUEURL { get; set; }
+        public static RegionEndpoint REGION { get; set; }
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new lblOutput());
+            GetSettings();
+        }
+
+        static void GetSettings()
+        {
+            QUEUE = ConfigurationManager.AppSettings.Get("queue-name");
+            QUEUEURL = ConfigurationManager.AppSettings.Get("queue-url");
+            REGION = RegionEndpoint.USEast1;
         }
     }
 }
--- Learn-Amazon-SQS/SqsService.cs
+++ Learn-Amazon-SQS/SqsService.cs
@@ -6,19 +6,18 @@ using System.Threading.Tasks;
 using Amazon.SQS;
 using System.Configuration;
 using Amazon;
-
+using System.Configuration;
 
 namespace Learn_Amazon_SQS
 {
     public class SqsService
     {
         public string QUEUE { get; set; }
+        public string QUEUEURL { get; set; }
         public RegionEndpoint REGION { get; set; }
-
-        public SqsService()
+        public SqsService(string url, string q, RegionEndpoint region)
         {
-            QUEUE = "";
-            REGION = RegionEndpoint.USEast1;
+            
         }
     }
 }
